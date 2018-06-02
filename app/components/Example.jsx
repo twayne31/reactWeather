b5def9a2b82106e05706725f73fbc8ef875dc962
Taylor Hampton <taylorhampton@Waynes-MacBook-Air.local>
2018-06-01 21:02:59 -0400

    update copy

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var Example = (props) => {
     return (
-        <h2>Example component</h2>
+        <h2>Example component!</h2>
     )
 }
 module.exports = Example;
