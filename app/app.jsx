572ec18c5418c52146aae663f3ab6a46296798af
Taylor Hampton <taylorhampton@Waynes-MacBook-Air.local>
2018-06-03 01:08:03 -0400

    add foundation and jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,12 @@ var Main = require('Main')
 var Weather = require('Weather');
 var About = require('About');
 var Example= require('Example');
+//load foundation
+
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
+
 
 ReactDOM.render(
   <Router history={hashHistory}>
